22d9f7b51cb8d98f4adf54d002e99a933a665afb
grant smith <grantsmith1256@gmail.com>
2018-11-04 21:37:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = 
   name_hash.each do |key, value|
+    binding.pry
     if lowest_value == 0
       key = lowest_key
       value = lowest_value
