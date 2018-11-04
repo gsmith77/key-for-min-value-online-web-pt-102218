1c1f79e9ce5208658301d5ae96eab046bb89f62c
grant smith <grantsmith1256@gmail.com>
2018-11-04 21:35:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_value = 0
+  lowest_key = 
+  name_hash.each do |key, value|
+    if lowest_value == 0
+      key = lowest_key
+      value = lowest_value
+      elsif lowest_value > value 
+      value = lowest_value
+      key = lowest_key
+    end
+  end
+  lowest_key
 end
